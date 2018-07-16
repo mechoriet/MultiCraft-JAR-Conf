f923ea003c6b0c8c5dba2fbc97df39c047e33a24
mechoriet <kevin@diviwork.nl>
2018-07-16 14:08:55 +0200

    added new packages

---
+++ scripts/install-centrix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Spigot config get
+echo -e "
+  __  __       _ _   _                 __ _                 _   _    ____     ____             __ _       
+ |  \/  |_   _| | |_(_) ___ _ __ __ _ / _| |_              | | / \  |  _ \   / ___|___  _ __  / _(_) __ _ 
+ | |\/| | | | | | __| |/ __|  __/ _  | |_| __|  _____   _  | |/ _ \ | |_) | | |   / _ \|  _ \| |_| |/ _  |
+ | |  | | |_| | | |_| | (__| | | (_| |  _| |_  |_____| | |_| / ___ \|  _ <  | |__| (_) | | | |  _| | (_| |
+ |_|  |_|\__,_|_|\__|_|\___|_|  \__,_|_|  \__|          \___/_/   \_\_| \_\  \____\___/|_| |_|_| |_|\__, |
+                                                                                                    |___/ "
+echo -e "\033[0;36m ## \033[0;32mLet's start getting all this things baby ! \033[37m"
+wget -N -P ./jar https://git.diviwork.nl/mechoriet/multicraft-conf/raw/master/minecraft/paperspigot/paperspigot-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/paperspigot-1.12.2.jar.conf
+
+wget -N -P ./jar https://git.diviwork.nl/mechoriet/multicraft-conf/raw/master/minecraft/bungeecord/bugeecord-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/bugeecord-1.12.2.jar.conf
+
+wget -N -P ./jar https://git.diviwork.nl/mechoriet/multicraft-conf/raw/master/minecraft/spigot/spigot-1.12.2.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.12.2.jar.conf
+
+wget -N -P ./jar https://git.diviwork.nl/mechoriet/multicraft-conf/raw/master/minecraft/spigot/spigot-1.13-pre7.jar.conf
+chown minecraft:minecraft ./jar/spigot-1.13-pre7.jar.conf
+
+echo -e "\033[0;36m ## \033[0;35mPermissions accorded my general ! \033[0m"
+echo -e "\033[0;36m ## \033[0;32mWell well well, that's the end you get all \033[0;33mSPIGOT \033[0;32mfiles ! Great ! \033[37m"
+echo -e "\033[0;36m ## \033[5;32mGoodbye ! \033[0m"
+rm -r install-centrix.sh
+exit
